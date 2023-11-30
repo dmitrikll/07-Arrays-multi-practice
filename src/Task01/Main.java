03c973839cb2bd46bdcd55de7a4601ab8a407052
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-11-30 20:31:37 +0200

    add task01 solution

---
+++ src/Task01/Main.java
@@ -0,0 +1,29 @@
+package Task01;
+
+public class Main {
+
+    static String[][] orderList;
+
+    public static void main(String[] args) {
+
+        orderList = new String[][]{
+                {"apple", "mango", "kiwi"},
+                {"potato", "tomato", "carrot", "cucumber"},
+                {"sugar", "salt"}
+        };
+
+        System.out.println("Order list: ");
+
+        int count = 0;
+
+        for (int i = 0; i < orderList.length; i += 1) {
+            count += 1;
+            System.out.print("Order #" + count + ": ");
+
+            for (int j = 0; j < orderList[i].length; j += 1) {
+                System.out.print(orderList[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
